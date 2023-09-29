7e106da6fcdaf1fe7cc2acaeaad734d2ea7f0a27
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-09-29 00:11:14 -0600

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <SDL2/SDL.h>
+#include <SDL_render.h>
+#include <SDL_video.h>
+
+#include "raycaster.h"
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("DOOM", 0, 0, 1000, 1000, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+                break;
+            }
+        }
+    //    SDL_RenderPresent 
+    }
+    
+
+}
