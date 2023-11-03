5d71d3d56f546b29bf66f1b1dd346c37672ccc2c
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-11-02 20:57:58 -0600

    Udated code with example from class

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <SDL2/SDL.h>
+#include <SDL_events.h>
 #include <SDL_render.h>
 #include <SDL_video.h>
 
+#include "color.h"
 #include "raycaster.h"
 
-SDL_Window* window;
-SDL_Renderer* renderer;
+SDL_Window *window;
+SDL_Renderer *renderer;
+
+void clear() {
+    SDL_SetRenderDrawColor(renderer, B.r, B.g, B.b, B.a);
+    SDL_RenderClear(renderer);
+}
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
 
-    window = SDL_CreateWindow("DOOM", 0, 0, 1000, 1000, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("DOOM", 0, 0, 1000, 500, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    Raycaster r = {renderer};
+    r.load_map("../assets/map.txt");
 
     bool running = true;
     while (running) {
@@ -21,14 +31,38 @@ int main() {
                 running = false;
                 break;
             }
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                case SDLK_LEFT:
+                    r.player.x -= 10;
+                    break;
+                case SDLK_RIGHT:
+                    r.player.x += 10;
+                    break;
+                case SDLK_UP:
+                    r.player.y -= 10;
+                    break;
+                case SDLK_DOWN:
+                    r.player.y += 10;
+                    break;
+                case SDLK_a:
+                    r.player.a += 3.14 / 12;
+                    break;
+                case SDLK_d:
+                    r.player.a -= 3.14 / 12;
+                    break;
+                default:
+                    break;
+                }
+            }
         }
-        // Present the framebuffer to the screen
+
+        clear();
+        r.render();
+
         SDL_RenderPresent(renderer);
     }
 
-    // Quit SDL
-    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
 }
